ccd26106c7bb964d0f5f0f78e1a02f1b1d4d1d89
JoshGB642 <32041132+JoshGB642@users.noreply.github.com>
2021-01-14 21:28:12 +0000

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const TITLE = "Ultra Crazy Cyclone Simulator";
-const VERSION_NUMBER = "0.4.1.4";
+const VERSION_NUMBER = "0.4.1.4a";
 
 const SAVE_FORMAT = 6;  // Format #6 in use starting in v0.2
 const EARLIEST_COMPATIBLE_FORMAT = 0;
